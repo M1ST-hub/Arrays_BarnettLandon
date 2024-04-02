540bd8b17502c3bb3a18960149187da8e03c69ad
M1ST-hub <nmckrypt@gmail.com>
2024-04-02 10:32:13 -0500

    Initial commit

---
+++ Arrays_BarnettLandon/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Arrays_BarnettLandon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks user to specify how many elements in the array
+            Console.WriteLine("Enter a whole number for the array size.");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[arraySize];
+            int sum = 0;
+
+            //Asks user to input values for all array elements
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Enter a whole number to add to the array.");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                sum += numbers[i];
+            }
+            //Sum of all elements in the array
+            Console.WriteLine($"The sum of the array is {sum}\n");
+            
+            Array.Sort(numbers);
+
+            Console.WriteLine("Array:");
+            foreach( int number in numbers ) 
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
